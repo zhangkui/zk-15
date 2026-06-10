f32a8a56c0b540e78a6a3b926cfc3cc478ed2ba1
团子 <876924196@qq.com>
2026-06-10 20:04:39 +0800

    1

--- backend/src/main/java/com/waste/mapper/WasteCategoryMapper.java
+++
@@ -1,9 +0,0 @@
-package com.waste.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.waste.entity.WasteCategory;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface WasteCategoryMapper extends BaseMapper<WasteCategory> {
-}
